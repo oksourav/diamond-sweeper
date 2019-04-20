2845e1bf3b2e32e9d77dd89dd23e38d1f1248d66
sourav52 <souravkumar@aahaastores.com>
2019-04-21 02:20:52 +0530

    Updates

---
+++ precache-manifest.27ddc342f8c15e2ac03f86d935dcbdc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad51cf75b3d2f1371f78600cc12ec59a",
+    "url": "/sourav52/diamond-sweeper/static/media/award.ad51cf75.svg"
+  },
+  {
+    "revision": "bac836effa4273776866",
+    "url": "/sourav52/diamond-sweeper/static/js/runtime~main.9afdfb87.js"
+  },
+  {
+    "revision": "c44bb86350e2d4301456",
+    "url": "/sourav52/diamond-sweeper/static/js/main.1837e50d.chunk.js"
+  },
+  {
+    "revision": "90318836401264968490",
+    "url": "/sourav52/diamond-sweeper/static/js/2.c3d829ef.chunk.js"
+  },
+  {
+    "revision": "c44bb86350e2d4301456",
+    "url": "/sourav52/diamond-sweeper/static/css/main.c43ca4d5.chunk.css"
+  },
+  {
+    "revision": "90318836401264968490",
+    "url": "/sourav52/diamond-sweeper/static/css/2.b84e4732.chunk.css"
+  },
+  {
+    "revision": "728ef53ace74ff4deef4677db7929597",
+    "url": "/sourav52/diamond-sweeper/index.html"
+  }
+];
